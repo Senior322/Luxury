9a20d227f68df8af6a8a53b3c71aa9d61ace06cb
Senior322 <alexey.k1717@gmail.com>
2023-07-24 23:53:48 +0300

    make burger animation

--- src/js/header.js
+++ src/js/header.js
@@ -2,6 +2,7 @@ const menuButton = document.querySelector('.button__menu');
 const Burger = document.querySelector('.burger');
 
 menuButton.addEventListener('click', (e)=>{
-   Burger.classList.toggle('burger__open')
-   document.body.classList.toggle('offScroll')
+   Burger.classList.toggle('burger__open');
+   document.body.classList.toggle('offScroll');
+   menuButton.classList.toggle('button__menu--close')
 });
