1db66b4b51b0d5008b417272e8f6eab6aeeec15f
Senior322 <alexey.k1717@gmail.com>
2023-07-30 15:56:34 +0300

    next section

--- src/js/Projects.js
+++ src/js/Projects.js
@@ -1,7 +1,7 @@
 const Data = document.querySelector('.projects__menu-images');
 const nextButtons = document.querySelectorAll('.projects__pagination-li');
-const previousButton = document.querySelector('.test2');
-const button3 = document.querySelector('.test0');
+// const previousButton = document.querySelector('.test2');
+// const button3 = document.querySelector('.test0');
 
 nextButtons.forEach((button ,index, arr)=>{
    button.addEventListener('click', ()=> {
@@ -45,99 +45,4 @@ function previousPage() {
    }
 }
 showCurrentPage(1);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let currentPageIndex = 0;
-// const itemsPerPage = 4;
-
-
-// function showCurrentPage(){
-//    const allItems = Data.querySelectorAll('.projects__menu-images-li');
-
-
-//    allItems.forEach(item =>{
-//       item.style.display = 'none';
-//    });
-
-//    const startIndex = currentPageIndex * itemsPerPage;
-//    const endIndex = startIndex + itemsPerPage;
-
-//    for(let i = startIndex; i < endIndex; i++) {
-//       if(allItems[i]){
-//          allItems[i].style.display = 'block';
-//       };
-//    };
-
-//    updateButtonsState();
-// };
-
-// function nextPage(){
-//    if(currentPageIndex < Math.ceil(Data.querySelectorAll('.projects__menu-images-li').length / itemsPerPage) - 1){
-//       currentPageIndex++;
-//       showCurrentPage();
-//    }
-// };
-
-// function previousPage() {
-//    if (currentPageIndex > 0) {
-//        currentPageIndex--;
-//        showCurrentPage();
-//    }
-// };
-
-// function updateButtonsState(){
-//    if (currentPageIndex === Math.ceil(Data.querySelectorAll('.projects__menu-images-li').length / itemsPerPage) - 1) {
-//       nextButton.disabled = true;
-//    } else {
-//       nextButton.disabled = false;
-//    }
-//   if (currentPageIndex === 0) {
-//       previousButton.disabled = true;
-//    } else {
-//       previousButton.disabled = false;
-//    }
-// }
-
-// showCurrentPage();
-// previousButton.disabled = true;
-// updateButtonsState();
-
-
-
-
-//    function main() {
-//       let currentPage = 0;
-//       let items = 3;
-
-//    function displayList(arrData, itemPerPage, page){
-//       const start = itemPerPage * page;
-//       const end = start + itemPerPage;
-//       const paginatedData = arrData.slice(start, end);
-
-//       paginatedData.forEach((el) => {
-
-//       });
-//    };
-
-//    function displayPagination(){};
-
-//    function displayPaginationButton(){};
-//    displayList(Data, items, currentPage);
-// }
-// main();
 
